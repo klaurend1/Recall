136a0834486457ce2c93119be44cbc68cf7167fa
Keith Laurendine <keith.laurendine@gmail.com>
2026-06-08 14:26:02 -0500

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by Keith Laurendine Jr on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
